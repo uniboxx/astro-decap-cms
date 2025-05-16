3d3d203786471abbb077cf735331e57bad19441e
unibox <unibox@duck.com>
2025-05-16 16:34:33 +0200

    add reading time and other stuff

--- src/assets/lib/utils.ts
+++ src/assets/lib/utils.ts
@@ -1,11 +1,19 @@
 export function createExcerpt(post: any) {
   const firstParagraph = post.body.match(/^\w+[\w\W]+/m)[0];
-  // console.log(firstParagraph);
-  const first150 = firstParagraph
-    .replace(/[^\w\s\.,!\?\n]+/gm, '')
-    .substring(0, 150);
-  // console.log(first150);
-  const excerpt = first150.replace(/[\w\.?]+$/m, '').trim() + ' ...';
+  console.log(firstParagraph);
+  const first30Words = firstParagraph
+    .replace(/[^\w\s\.,!?]+/gm, '')
+    .split(/\s+/)
+    .slice(0, 30)
+    .join(' ');
+  console.log(first30Words);
+  const excerpt = first30Words.replace(/\.+$/, '').trim() + '...';
   console.log(excerpt);
   return excerpt;
+}
+
+export function readingTime(article: string) {
+  const wpm = 225;
+  const words = article.trim().split(/\s+/).length;
+  return Math.ceil(words / wpm);
 }
